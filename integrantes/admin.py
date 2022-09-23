d2b06c04590c5fdaf17a68a444cd6c6944c1220e
LucianoBauso <83524525+LucianoBauso@users.noreply.github.com>
2022-09-23 00:56:52 -0300

    Delete integrantes directory

--- integrantes/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Casa, Integrante, Auto
-
-# Register your models here.
-admin.site.register(Integrante)
-admin.site.register(Auto)
-admin.site.register(Casa)
--- integrantes/forms.py
+++
@@ -1,8 +0,0 @@
-from socket import fromshare
-from django import forms
-
-class IntegranteFormulario(forms.Form):
-    nombre = forms.CharField(max_length=15)
-    apellido = forms.CharField(max_length=15)
-    edad = forms.IntegerField()
-    fecha_nacimiento = forms.DateField()
